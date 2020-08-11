5009e5174d51df341673f823c0dcd070b320809d
Johee Gloria Chung <johee@MININT-CVD245S.redmond.corp.microsoft.com>
2020-08-10 19:42:41 -0700

    Spike: Authorize Request via redirect to login

--- spikes/walmart-scim-spike/index.js
+++ spikes/walmart-scim-spike/index.js
@@ -51,6 +51,34 @@ function getInitialHtml() {
     }
 }
 
+// Login must be user of Application
+function getRedirectLogin() {
+    // From Enterprise Application
+    // const azureTenantId = '9c26242b-1b00-450c-98b0-a31412ad5a0e';
+    // const spClientId = 'd08b61d1-a22f-47ef-bec2-24c0016579ef';
+    // const clientSecret = 'dapi5b33859ef12f7039b937146175a9d313';
+
+    // From App registrations
+    const azureTenantId = '9c26242b-1b00-450c-98b0-a31412ad5a0e'
+    const spClientId = '6ff577ed-e44b-4f35-88a9-12cbafd2f597';
+    const clientSecret = 'b.-CfBPGpr11dfOYL~C4xVjQ1-1b5e1F_d';
+
+    // Sign in request URI
+    const params = [
+        { key: 'client_id', value: spClientId },
+        { key: 'response_type', value: 'code' },
+        // reply URL needs to be configured for application
+        { key: 'redirect_uri', value: 'http://localhost:1337' },
+        { key: 'response_mode', value: 'query' },
+        { key: 'scope', value: 'openid%20offline_access%20https%3A%2F%2Fgraph.microsoft.com%2F.default' },
+        { key: 'state', value: 12345 },
+    ];
+
+    const redirectUrl = `https://login.microsoftonline.com/${azureTenantId}/oauth2/v2.0/authorize?${params.map(({ key, value }) => `${key}=${value}`).join('&')}`
+
+    return { response: redirectUrl, status: 302 };
+}
+
 // Currently only handles GET method
 async function databricksUsers(method, query) {
     const { databricksDomainName } = query;
@@ -88,6 +116,8 @@ async function getPathResponse(req) {
     switch (pathname) {
         case '/':
             return getInitialHtml();
+        case '/login':
+            return getRedirectLogin();
         case '/databricksUsers':
             return databricksUsers(method, query);
         default:
@@ -98,10 +128,37 @@ async function getPathResponse(req) {
 const server = http.createServer();
 server.on('request', async (req, res) => {
     const { response, status, contentType } = await getPathResponse(req);
-    res.writeHead(status, { ...(contentType && { 'Content-Type': contentType }) })
-    res.write(response);
+    if (status === 302 ) {
+        res.writeHead(302, { Location: response });
+    } else {
+        res.writeHead(status, { ...(contentType && { 'Content-Type': contentType }) })
+        res.write(response);
+    }
     res.end();
 });
+
+// fetch('https://login.microsoftonline.com/9c26242b-1b00-450c-98b0-a31412ad5a0e/oauth2/v2.0/token', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/x-www-url-form-urlencoded'},
+//     body: {
+//         client_id: 'd08b61d1-a22f-47ef-bec2-24c0016579ef',
+//         scope: 'https://graph.microsoft.com/mail.read',
+//         redirect_uri: 'https://localhost',
+//         grant_type: 'authorization_code',
+//         client_secret: 'dapi5b33859ef12f7039b937146175a9d313',
+//         code: 'OAQABAAIAAAAm-06blBE1TpVMil8KPQ41EJlDYDIEO6wVGDqjzf3ouDkqcsY7U66lbqMEZRlMEl02Jajs9FbrhlHLNj3kVSBxywXaYFa-lJFOT2MLi-1eAUAg_gz5OmbhvudN6IINFNv_sb0Rw34teK4v7KJIDFUWIdOdLPkx3vGxZ4o-l7ADvqUfUhqKvrqaTF66o_b8hHS_ZP5nl7QVtbp8Ycwvo9EJlX8-rAEBanpnazmmppMw3TJsG_XsaNCkT0Q2X3sD6CLmHTs3pgcA65V0ZQTCavDglSMfIj5pCOwf_D-2QM1Kj3inoQ6R-2t8rUni1WcVz4AewfTCjF_acDTTJMEtN6-SYenuaegfv95pj9dATnrgj1VCifkCzF781WuPl1BTFdccumgp_u6MsNVCOKkA3spf3TeUUAnFkum3cu-PUIfOlvHnnA4eyo67c0kq2Gcy9j5IAfIMxn9YWZDIMqFb_GCRJYM7eQ2g4QxHiHvaInkGa-TL4Hn_7toQGQnpfzaL-PUP4dOqL3u2VQCDVOxU4szSNhnbRNlLkI8ZPTTdzfWEZnwcwNTXsFn1OZ02E1IWalQjskatrdVTWQJBDrPsRyV9Vif7dwrK1DXBLrFGJeUzKPvH2SdfkEp89QVpgESwHCQgAA',
+//     },
+// })
+//     .then(res => console.log({ res }))
+//     .catch(err => console.log({ err }));
+
+// fetch('https://graph.microsoft.com/beta/applicationTemplates', {
+//     method: 'GET',
+//     headers: { Authorization: 'Bearer dapi5b33859ef12f7039b937146175a9d313' }
+// })
+//     .then(res => console.log({ res }))
+//     .catch(err => console.log({ err }));
+
 const port = process.env.PORT || 1337;
 server.listen(port);
 console.log("Server running at http://localhost:%d", port);
