2722ef35ac94836c4f8718c6913bb940befb4439
alfredoihernandez <a14hdz@gmail.com.com>
2020-08-25 13:24:52 -0500

    add refreshtoken functionality

--- spikes/walmart-scim-spike/index.js
+++ spikes/walmart-scim-spike/index.js
@@ -135,6 +135,50 @@ async function postAccessToken(req) {
     }
 }
 
+async function postRefreshAccessToken(req) {
+    try {
+        const formParams = [
+            { key: 'client_id', value: spClientId },
+            { key: 'scope', value: 'https://graph.microsoft.com/mail.read' },
+            { key: 'redirect_uri', value: getOriginUrl(req) },
+            { key: 'grant_type', value: 'refresh_token' },
+            { key: 'client_secret', value: clientSecret },
+            { key: 'refresh_token', value: req.headers.authorization },
+        ].map(({ key, value }) => encodeURIComponent(key) + '=' + encodeURIComponent(value)).join('&');
+
+        // TODO: This is the token they'll have in key vault (would need refresh mechanism)
+        // TODO: May use certificate instead of token
+        const response = await fetch(`https://login.microsoftonline.com/${azureTenantId}/oauth2/v2.0/token`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: formParams,
+        });
+        const contentType = response.headers._headers['content-type'];
+        const body = contentType.some(type => type.includes('json')) ? await response.json() : await response.text();
+        if (response.status === 200) {
+            return {
+                status: response.status,
+                contentType,
+                response: JSON.stringify(body),
+            };
+        } else {
+            return {
+                status: response.status,
+                contentType,
+                response: JSON.stringify(body),
+            };
+        }
+    } catch(err) {
+        const errorMessage = 'Error fetching tokens';
+        console.error(errorMessage + ': ', err);
+        return {
+            response: errorMessage,
+            status: 500,
+            contentType: 'text/plain',
+        }
+    }
+}
+
 async function postDatabricksGalleryApp(req) {
     try {
         const { query: { scimAppTemplateId, scimConnectorGalleryAppName } } = url.parse(req.url, true);
@@ -513,6 +557,8 @@ async function postRoute(req, res) {
     switch (pathname) {
         case '/accessToken':
             return sendResponse(res, await postAccessToken(req));
+        case '/refreshAccessToken':
+            return sendResponse(res, await postRefreshAccessToken(req));
         case '/databricksGalleryApp':
             return sendResponse(res, await postDatabricksGalleryApp(req));
         case '/aadGroupToScim':
