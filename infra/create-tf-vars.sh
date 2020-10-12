4f4ac7e19d7a1d7cd013886d311a6f4f061aa715
Kushal Azim Ekram <mekram@microsoft.com>
2020-10-12 11:51:56 -0500

    `create-tf-vars.sh` script enhancement (#90)
    * Enhancement: added special characters check on scim_Name. Independent from examples.tfvars. Backup previous terraform.tfvars
    
    * Used cat for backup

--- infra/create-tf-vars.sh
+++ infra/create-tf-vars.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # check if scim_Name is valid
-
+function usage(){
+  echo -e "usage: scim_Name=[unique-name] $0"
+  echo -e "   or: export scim_Name=[unique-name] && $0"
+  echo -e "Note that: 'scim_Name' can't have any special chars with length between 5-20"
+}
+echo "SCIM-Name provided: '$scim_Name'"
+script_dir=$(dirname $(realpath $0))
+tf_file=${script_dir}/terraform.tfvars
+pat_special_chars="[!@#$%^&*()+,.?~\":{}|<> ]"
 Name_Size=${#scim_Name}
-if [[ $Name_Size -lt 5 || $Name_Size -gt 20 ]]
-then
-  echo "Please set scim_Name first and make sure it is between 5 and 20 characters in length with no special characters."
-  echo $scim_Name
-  echo $Name_Size
+if [[ $Name_Size -lt 5 || $Name_Size -gt 20 || "$scim_Name" =~ ${pat_special_chars} ]]; then
+  usage
   exit 1
 fi
-
-
-
+#exit
 # set location to centralus if not set
-if [ -z $scim_Location ]
-then
-  export scim_Location=centralus
-fi
+[[ -z $scim_Location || ${#scim_Location} == 0 ]] && scim_Location=southcentralus
 #  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>TODO <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 # set repo to databricks-scrime-automation if not set
 # if [ -z $scim_Repo ]
@@ -27,44 +27,39 @@ fi
 
 # create terraform.tfvars and replace template values
 
+# Template from example.tfvars
+#LOCATION         = "<<scim_Location>>"
+#NAME             = "<<scim_Name>>"
+#TF_SUB_ID        = "<<scim_SUB_ID>>"
+#TF_CLIENT_ID     = "<<scim_CLIENT_ID>>"
+#TF_CLIENT_SECRET = "<<scim_CLIENT_SECRET>>"
+#TF_TENANT_ID     = "<<scim_TENANT_ID>>"
+#COSMOS_RU        = "1000"
 
 
+LOCATION="$scim_Location"
+NAME="$scim_Name"
+TF_TENANT_ID="$(az account show -o tsv --query tenantId)"
+TF_SUB_ID="$(az account show -o tsv --query id)"
+TF_CLIENT_SECRET="$(az ad sp create-for-rbac -n http://${scim_Name}-tf-sp --query password -o tsv)"
+TF_CLIENT_ID="$(az ad sp show --id http://${scim_Name}-tf-sp --query appId -o tsv)"
+COSMOS_RU=1000
+#cp example.tfvars terraform.tfvars
+# Backup previous terraform.tfvars file is present
+if [[ -f "${tf_file}" ]]; then
+  bak="${tf_file}.bak.$(date +%s)"
+  cat "${tf_file}" >| $bak
+  echo "Backing up $(basename ${tf_file}) file to $bak"
+fi
 
-
-
-cp example.tfvars terraform.tfvars
-
-# replace name
-ex -s -c "%s/<<scim_Name>>/$scim_Name/g|x" terraform.tfvars
-
-# replace location
-ex -s -c "%s/<<scim_Location>>/$scim_Location/g|x" terraform.tfvars
-
-# replace repo
-# ex -s -c "%s/<<scim_Repo>>/$scim_Repo/g|x" terraform.tfvars
-
-# replace email
-# ex -s -c "%s/<<scim_Email>>/$scim_Email/g|x" terraform.tfvars
-
-# replace TF_TENANT_ID
-ex -s -c "%s/<<scim_TENANT_ID>>/$(az account show -o tsv --query tenantId)/g|x" terraform.tfvars
-
-# replace TF_SUB_ID
-ex -s -c "%s/<<scim_SUB_ID>>/$(az account show -o tsv --query id)/g|x" terraform.tfvars
-
-# create a service principal
-# replace TF_CLIENT_SECRET
-ex -s -c "%s/<<scim_CLIENT_SECRET>>/$(az ad sp create-for-rbac -n http://${scim_Name}-tf-sp --query password -o tsv)/g|x" terraform.tfvars
-
-# replace TF_CLIENT_ID
-ex -s -c "%s/<<scim_CLIENT_ID>>/$(az ad sp show --id http://${scim_Name}-tf-sp --query appId -o tsv)/g|x" terraform.tfvars
-
-# create a service principal
-# replace ACR_SP_SECRET
-# ex -s -c "%s/<<scim_ACR_SP_SECRET>>/$(az ad sp create-for-rbac --skip-assignment -n http://${scim_Name}-acr-sp --query password -o tsv)/g|x" terraform.tfvars
-
-# replace ACR_SP_ID
-# ex -s -c "%s/<<scim_ACR_SP_ID>>/$(az ad sp show --id http://${scim_Name}-acr-sp --query appId -o tsv)/g|x" terraform.tfvars
-
+# Output to ${tf_file}
+echo Writing to ${tf_file}
+printf '%-20s= "%s"\n' LOCATION $LOCATION >| ${tf_file} # --> Make sure we're overwriting the file with >|
+printf '%-20s= "%s"\n' NAME $NAME >> ${tf_file}
+printf '%-20s= "%s"\n' TF_SUB_ID $TF_SUB_ID >> ${tf_file}
+printf '%-20s= "%s"\n' TF_TENANT_ID $TF_TENANT_ID >> ${tf_file}
+printf '%-20s= "%s"\n' TF_CLIENT_ID $TF_CLIENT_ID >> ${tf_file}
+printf '%-20s= "%s"\n' TF_CLIENT_SECRET $TF_CLIENT_SECRET >> ${tf_file}
+printf '%-20s= "%s"\n' COSMOS_RU $COSMOS_RU >> ${tf_file}
 # validate the substitutions
-cat terraform.tfvars
+cat ${tf_file}
